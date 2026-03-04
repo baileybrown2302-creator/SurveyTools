7949c3f2db0c6bc8becb9321cd9d932fda01c990
baileybrown2302-creator <bailey.brown.2302@gmail.com>
2026-03-04 15:16:21 -0800

    beta branch & materials

---
+++ beta/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'survey-tool-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './leveling.html',
+  './manifest.json',
+  './icon.png'
+  './materials.html'
+];
+
+// Install: cache all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serve from cache first, fall back to network, update cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const fetchPromise = fetch(event.request).then(response => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
